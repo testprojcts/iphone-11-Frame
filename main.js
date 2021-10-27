753040351d15962962e9ecd6a6268e59188f25e8
Test Projects 83 <testprojects83@gmail.com>
2021-10-27 17:55:46 +0100

    updated

--- main.js
+++ main.js
@@ -4,13 +4,11 @@ user_btn = document.getElementById('user');
 const user_data = [];
 
 user_btn.addEventListner('click', () => {
-  alert('User button clicked');
+  getData();
+  user_data.push(getData);
 });
 
 function getData() {
   let name = prompt('What is your name?');
   let age = prompt('What is your age?');
 }
-
-getData();
-user_data.push(getData);
