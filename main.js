22dd1abc31ca8600f1c2a3fc6d916d2280661d7c
Test Projects 83 <testprojects83@gmail.com>
2021-10-27 17:44:58 +0100

    minor

--- main.js
+++ main.js
@@ -6,3 +6,11 @@ const user_data = [];
 user_btn.addEventListner('click', () => {
   alert('User button clicked');
 });
+
+function getData() {
+  let name = prompt('What is your name?');
+  let age = prompt('What is your age?');
+}
+
+getData();
+user_data.push(getData);
